1be3059cbcfde940dbaa16f5a1ed68c7c7540a1c
barry-far <143993892+barry-far@users.noreply.github.com>
2023-10-27 21:07:24 +0330

    Higher Speed 🚅🚤⚡

--- js/zero.js
+++ js/zero.js
@@ -1,6 +1,4 @@
- /* --------------------------------------------------
-  * © Copyright 2021 - Hostco by Designesia
-  * --------------------------------------------------*/
+
 (function($) {
 	'use strict';
 
@@ -1328,13 +1326,6 @@
          });
      }
 
-     jQuery("#dark-mode").on("click", function() {
-        if(jQuery('body').hasClass('dark-scheme')){
-            window.location.href = 'https://www.designesia.com/themes/gospace/index.html';
-        }else{
-            window.location.href = 'https://www.designesia.com/themes/gospace/02_dark-index.html';
-        }
-     });
 
 	 function grid_gallery() {
             jQuery('.grid-item').each(function () {
